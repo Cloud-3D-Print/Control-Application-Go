0d1e705629c726ca77608f4cc3a939520cfc4270
akiencore <akiencore@gmail.com>
2022-09-13 10:29:30 -0400

    fix bloacked script error

--- install.sh
+++ install.sh
@@ -27,11 +27,11 @@ echo "Installing pi printer controller service"
 chmod +x /home/${SVCUSER}/control/piPrinterCtrl/install_piPrinterCtrl_systemd.sh
 chmod +x /home/${SVCUSER}/control/piPrinterCtrl/piPrinterCtrl
 cd /home/${SVCUSER}/control/piPrinterCtrl/
-./install_piPrinterCtrl_systemd.sh >> /dev/null 2>&1
+./install_piPrinterCtrl_systemd.sh &>/dev/null || true
 
 
 echo "Installing tpcpi local video & AI sync service"
 chmod +x /home/${SVCUSER}/control/tpcpilocal/install_tpcpilocal_systemd.sh
 chmod +x /home/${SVCUSER}/control/tpcpilocal/tpcpilocal
 cd /home/${SVCUSER}/control/tpcpilocal/
-./install_tpcpilocal_systemd.sh >> /dev/null 2>&1
+./install_tpcpilocal_systemd.sh &>/dev/null || true
