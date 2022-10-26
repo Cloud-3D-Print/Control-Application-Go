1ab8ccd21a0f49a2f145cdb89dd99e5a7b255993
akiencore <akiencore@gmail.com>
2022-10-26 10:30:55 -0400

    new releases

--- install.sh
+++ install.sh
@@ -9,27 +9,21 @@ then
   read -p "Do you want to reinstall? (y|n)" -n 1 -r
   if [[ $REPLY =~ ^[Yy]$ ]]
     then
-      # sudo rm -rf /home/${SVCUSER}/control
-      echo "Reinstalling Control Application"
+      sudo rm -rf /home/${SVCUSER}/control
+      # echo "Reinstalling Control Application"
     else
       exit 0
   fi
 fi
 
-cd ~
-mkdir -p ~/Downloads
-cd Downloads
-curl -L -O https://github.com/mech-soluitons-ltd/Control-Application-Go/releases/download/v1.0.1/control_app_rpi.tar.gz
-mkdir -p ~/Downloads/control
-tar xzf control_app_rpi.tar.gz -C ./control
+# cd ~
+# make -p Downloads
+# cd Downloads
 
-cp -r -f ~/Downloads/control ~
+echo "Installing Git"
+sudo apt-get install git -y
 
-
-# echo "Installing Git"
-# sudo apt-get install git -y
-
-# git clone https://github.com/mech-soluitons-ltd/Control-Application-Go.git /home/${SVCUSER}/control
+git clone https://github.com/mech-soluitons-ltd/Control-Application-Go.git /home/${SVCUSER}/control
 
 
 
