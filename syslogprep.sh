fb8214abd5460ff31a25c5e5b8e9be2c565828bb
akiencore <akiencore@gmail.com>
2022-11-04 12:45:54 -0400

    new releases

--- syslogprep.sh
+++ syslogprep.sh
@@ -1,3 +1,38 @@
 #!/usr/bin/bash
 set -e
 
+touch /tmp/rsyslog
+cat <<EOF > /tmp/rsyslog
+/var/log/syslog
+/var/log/mail.info
+/var/log/mail.warn
+/var/log/mail.err
+/var/log/mail.log
+/var/log/daemon.log
+/var/log/kern.log
+/var/log/auth.log
+/var/log/user.log
+/var/log/lpr.log
+/var/log/cron.log
+/var/log/debug
+/var/log/messages
+{
+        size 200000
+        #rotate 4
+        rotate 3
+        #weekly
+        daily
+        missingok
+        notifempty
+        compress
+        #delaycompress
+        nodelaycompress
+        sharedscripts
+        postrotate
+                /usr/lib/rsyslog/rsyslog-rotate
+        endscript
+}
+
+EOF
+
+sudo mv -f /tmp/rsyslog /etc/logrotate.d/rsyslog
